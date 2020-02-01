603d8c2a24c3ee34e8e2566d809ca21d9afb774a
Goran Đukić <gdjukic@live.com>
2020-02-02 00:03:08 +0100

    Updates

---
+++ precache-manifest.48750acb0203ceec8b84e6de454395c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc6732872f4c7413d989b1ba1dc1ac0",
+    "url": "/react-store/index.html"
+  },
+  {
+    "revision": "4ea254de4990a7d6304d",
+    "url": "/react-store/static/css/main.857947bf.chunk.css"
+  },
+  {
+    "revision": "5495c7596fa56e0b7d65",
+    "url": "/react-store/static/js/2.5e28c3a8.chunk.js"
+  },
+  {
+    "revision": "4ea254de4990a7d6304d",
+    "url": "/react-store/static/js/main.5635cfc8.chunk.js"
+  },
+  {
+    "revision": "aeb9118edc9b5aa6be57",
+    "url": "/react-store/static/js/runtime~main.860989bb.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/react-store/static/media/logo.9f7e506d.svg"
+  }
+]);
